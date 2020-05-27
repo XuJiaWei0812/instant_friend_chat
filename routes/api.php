cdbbc5d4daa174152a6c5af32642c6a5d427a0d0
XuJiaWei0812 <58849785+XuJiaWei0812@users.noreply.github.com>
2020-05-27 22:30:03 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,32 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', function () {
+    return '登入API';
+});
+
+Route::post('/register', function () {
+    return '註冊API';
+});
+Route::get('/logout', function () {
+    return '登出API';
+});
+
+Route::group(['prefix' => 'friend'], function () {
+    Route::delete('/{friend_id}/delete', function () {
+        return '刪除好友&拒絕邀請API';
+    });
+    Route::post('/apply-for/{friend_id}', function () {
+        return '好友申請api';
+    });
+    Route::put('/apply-for/{friend_id}', function () {
+        return '同意好友申請api';
+    });
+    Route::post('/chat/{friend_id}', function () {
+        return '好友聊天api';
+    });
 });
