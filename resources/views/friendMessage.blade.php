41fbf50f24ffad372a25738a940a46acf0e23d7d
XuJiaWei0812 <58849785+XuJiaWei0812@users.noreply.github.com>
2020-05-27 22:29:52 +0800

    Create friendMessage.blade.php

---
+++ resources/views/friendMessage.blade.php
@@ -0,0 +1,13 @@
+@extends('layout/master_login')
+
+@section('title',$title)
+
+@section('content')
+<section class="container-fluid" id="show">
+    <div class="row mx-auto">
+        <div class="col-lg-7 mx-auto py-3">
+
+        </div>
+    </div>
+</section>
+@endsection
